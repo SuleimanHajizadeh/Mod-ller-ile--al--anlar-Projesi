fff9a60860020915a6c1d3c3bd5f394e55a95c27
software1engineer1Solomon <suleyman.hacizade1@gmail.com>
2023-06-07 13:35:45 +0400

    Changed and Learned

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-console.log("Test");
-console.log("Test2");
-console.log("Last ");
+import { Request } from "./requests";
+
+// Elementleri Secme
+const form = document.getElementById("employee-form");
+const nameInput = document.getElementById("name");
+const departmentInpun = document.getElementById("department");
+const salaryInput = document.getElementById("salary");
+const employeesList = document.getElementById("employees");
+const updateEmployeeButton = document.getElementById("update");
+
+const request = new Request("http://localhost:3000/employees")
+
+// request.get()
+// .then(employees => console.log(employees))
+// .catch(err => console.log(err));
+
+request.post({name: "Yahya Hajizadeh", department: "Pazarlama", salary:10000})
+.then(employee => console.log(employee))
+.catch(err => console.log(err));
