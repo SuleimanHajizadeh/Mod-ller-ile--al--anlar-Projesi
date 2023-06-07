2e7ec98586a71cbc990c11c0a28e25026d114f59
software1engineer1Solomon <suleyman.hacizade1@gmail.com>
2023-06-07 14:01:01 +0400

    Changed and Learned

--- src/index.js
+++ src/index.js
@@ -14,6 +14,14 @@ const request = new Request("http://localhost:3000/employees")
 // .then(employees => console.log(employees))
 // .catch(err => console.log(err));
 
-request.post({name: "Yahya Hajizadeh", department: "Pazarlama", salary:10000})
-.then(employee => console.log(employee))
+// request.post({name: "Yahya Hajizadeh", department: "Pazarlama", salary:10000})
+// .then(employee => console.log(employee))
+// .catch(err => console.log(err));
+
+// request.put(1,{name: "Yeshua Hajizadeh", department: "SnamiPharmacy LLC", salary:1000000})
+// .then(employee => console.log(employee))
+// .catch(err => console.log(err));
+
+request.delete(5)
+.then(message => console.log(message))
 .catch(err => console.log(err));
